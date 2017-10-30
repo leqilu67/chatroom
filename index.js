3d8b5d5b17b2870c00e21768c9f5ad7048e051d9
Leqi Lu <pototoland@gmail.com>
2017-10-30 15:19:40 -0700

    modified port

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -71,6 +73,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
